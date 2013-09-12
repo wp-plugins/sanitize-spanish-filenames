bcac23750f4e736ac1d86b4e89fe5bc81db4fa51
samuelaguilera <samuelaguilera@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-12 11:26:31 +0000

    Paso a estable versión 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/sanitize-spanish-filenames/trunk@770873 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,18 +1,21 @@
 === Sanitize Spanish Filenames ===
 Contributors: samuelaguilera
 Donate link: http://www.samuelaguilera.com
-Tags: utf-8, spanish, international characters, filename, nombre archivo, español, acentos
+Tags: utf-8, spanish, international characters, filename, nombre archivo, español, acentos, sanitize
 Requires at least: 3.0
-Tested up to: 3.4.2
-Stable tag: 1.0.2
+Tested up to: 3.6.1
+Stable tag: 1.0.3
+License: GPL2
 
-Filtra el nombre de los archivos sutituyendo los caracteres áéíóúüñºª y ÁÉÍÓÚÜÑ por aeiouunoa y AEIOUUN respectivamente.
+Filtra el nombre de los archivos sutituyendo o eliminando los carácteres especiales que contengan.
 
 == Description ==
 
-Filtra el nombre de los archivos que se suben a la librería multimedia, sutituyendo los caracteres áéíóúüñºª y ÁÉÍÓÚÜÑ por aeiouunoa y AEIOUUN respectivamente.
+Filtra el nombre de los archivos que se suben a la librería multimedia (no afecta a los archivos ya subidos), sutituyendo los siguientes caracteres:
 
-Esto hace que los nombres de los archivos sean más "amigables" con algunos servidores o incluso algunas extensiones de WordPress que no se llevan bien con los nombres de archivos que contienen carácteres internacionales como los usados en el idioma español.
+а б в г д е ж з и й к л м н о п р с т у ф х ц ч ш щ ь ю я ґ є і ї ё ы ъ э ў ą ć ę ł ń ó ś ź ż à á â ã ä å æ ç è é ê ë ì í î ï ð ñ ò ó ô õ ö × ø ù ú û ü ý þ ÿ ß ő ű ě š č ř ž ý á é ď ť ň ú ů
+
+Por alternativas que no causen las incompatibilidades que los carácteres especiales suelen provocar en los nombres de archivo.
 
 = Requirements =
 
@@ -31,8 +34,16 @@ Puede. Pero no ha sido probado. Funcionará siempre que el filtro y la función 
 = ¿Tienes previsto añadir otras funciones? =
 
 En principio no. Pero si se te ocurre alguna forma de mejorar esta extensión, puedes contactar conmigo a través del formulario de contacto que encontrarás en mi web.
+
+= Tengo muchos archivos ya subidos con carácteres especiales, ¿los modificará? =
+
+No. El filtro que realiza esta extensión se realiza durante la subida del archivo, y por tanto no afectará a los archivos subidos antes de activar la extensión.
                                   
 == Changelog ==
+
+= 1.0.3 =
+
+* Ampliado extensamente el juego de carácteres que se filtran. Limpiando así no sólo carácteres españoles sino de otros idiomas y algunos símbolos.
 
 = 1.0.2 =
 
@@ -45,5 +56,10 @@ En principio no. Pero si se te ocurre alguna forma de mejorar esta extensión, p
 = 1.0 =
 
 * Lanzamiento inicial.
+
+== Upgrade Notice ==
+
+= 1.0.3 =
+Actualización recomendada.
 
 
--- sanitize-spanish-filenames.php
+++ sanitize-spanish-filenames.php
@@ -3,7 +3,7 @@
 Plugin Name: Sanitize Spanish Filenames
 Plugin URI: http://www.samuelaguilera.com/archivo/filtrar-tildes-nombre-archivo-wordpress.xhtml
 Description: Filtra el nombre de los archivos que se suben a la librería multimedia, sutituyendo los caracteres áéíóúüñºª y ÁÉÍÓÚÜÑ por aeiouunoa y AEIOUUN respectivamente.
-Version: 1.0.2
+Version: 1.0.3
 Author: Samuel Aguilera
 Author URI: http://www.samuelaguilera.com
 License: GPL2
@@ -25,11 +25,70 @@ License: GPL2
 
 function sa_sanitize_spanish_chars ($filename) {
 
+		$original_chars = array(
+			'/А/','/Б/','/В/','/Г/', // cyrillic alphabet
+			'/Д/','/Е/','/Ж/','/З/','/И/',
+			'/Й/','/К/','/Л/','/М/','/Н/',
+			'/О/','/П/','/Р/','/С/','/Т/',
+			'/У/','/Ф/','/Х/','/Ц/','/Ч/',
+			'/Ш/','/Щ/','/Ь/','/Ю/','/Я/',
+			'/а/','/б/','/в/','/г/','/д/','/е/','/ж/',
+			'/з/','/и/','/й/','/к/','/л/',
+			'/м/','/н/','/о/','/п/','/р/',
+			'/с/','/т/','/у/','/ф/','/х/',
+			'/ц/','/ч/','/ш/','/щ/',
+			'/ь/','/ю/','/я/',
 
-$spanish_chars = array( '/á/', '/é/', '/í/', '/ó/', '/ú/', '/ü/', '/ñ/', '/Á/', '/É/', '/Í/', '/Ó/', '/Ú/', '/Ü/', '/Ñ/', '/º/', '/ª/' );
-$sanitized_chars = array('a', 'e', 'i', 'o', 'u', 'u', 'n', 'A', 'E', 'I', 'O', 'U', 'U', 'N', 'o', 'a');
+			'/Ґ/','/ґ/','/Є/','/є/','/І/','/і/','/Ї/','/ї/', // ukrainian
+			'/Ё/','/ё/','/Ы/','/ы/','/Ъ/','/ъ/','/Э/','/э/', // russian
+			'/Ў/','/ў/', // belorussian
+			'/Ä/','/ä/','/Ö/','/ö/','/Ü/','/ü/','/ß/', // german
+			'/Ą/','/ą/','/Ć/','/ć/','/Ę/','/ę/','/Ł/','/ł/','/Ń/','/ń/','/Ó/','/ó/','/Ś/','/ś/','/Ź/','/ź/','/Ż/','/ż/', // polish (new unique letters)
+			'/Ő/','/ő/','/Ű/','/ű/', // hungarian
+			'/ě/','/š/','/č/','/ř/','/ž/','/ý/','/á/','/é/','/ď/','/ť/','/ň/','/ú/','/ů/', // czech
+			'/Ě/','/Š/','/Č/','/Ř/','/Ž/','/Ý/','/Á/','/É/','/Ď/','/Ť/','/Ň/','/Ú/','/Ů/',
 
-$friendly_filename = preg_replace($spanish_chars, $sanitized_chars, $filename);
+			'/À/','/Á/','/Â/','/Ã/','/Å/','/Æ/','/Ç/','/È/','/É/','/Ê/','/Ë/','/Ì/','/Í/','/Î/','/Ï/','/Ð/','/Ñ/','/Ò/','/Ô/','/Õ/','/×/','/Ø/','/Ù/','/Ú/','/Û/','/Ý/','/Þ/', // extra all (http://www.atm.ox.ac.uk/user/iwi/charmap.html)
+			'/à/','/á/','/â/','/ã/','/å/','/æ/','/ç/','/è/','/é/','/ê/','/ë/','/ì/','/í/','/î/','/ï/','/ð/','/ñ/','/ò/','/ô/','/õ/','/×/','/ø/','/ù/','/ú/','/û/','/ý/','/þ/','/ÿ/','/Ÿ/',
+
+			'/№/','/“/','/”/','/«/','/»/','/„/','/@/','/%/', // other
+			'/‘/','/’/','/`/','/´/','/º/','/ª/'
+		);
+
+		$sanitized_chars = array(
+			'a','b','v','h', // cyrillic alphabet
+			'd','e','zh','z','y',
+			'j','k','l','m','n',
+			'o','p','r','s','t',
+			'u','f','h','c','ch',
+			'sh','shh','','ju','ja',
+			'a','b','v','h','d','e','zh',
+			'z','y','j','k','l',
+			'm','n','o','p','r',
+			's','t','u','f','h',
+			'c','ch','sh','sch',
+			'','ju','ja',
+
+			'g','g','je','je','i','i','ji','ji', // ukrainian
+			'jo','jo','y','y','','','ye','ye', // russian
+			'u','u', // belorussian
+			'ae','ae','oe','oe','ue','ue','ss', // german
+			'a','a','c','c','e','e','l','l','n','n','o','o','s','s','z','z','z','z', // polish
+			'o','o','u','u', // hungarian
+			'e','s','c','r','z','y','a','e','d','t','n','u','u',  // czech
+			'e','s','c','r','z','y','a','e','d','t','n','u','u',
+
+			'a','a','a','a','a','ae','c','e','e','e','e','i','i','i','i','d','n','o','o','o','x','o','u','u','u','y','p', // extra all
+			'a','a','a','a','a','ae','c','e','e','e','e','i','i','i','i','d','n','o','o','o','x','o','u','u','u','y','p','y','y',
+
+			'','','','','','','','', // other
+			'','','','','o','a'
+		);
+
+
+
+
+$friendly_filename = preg_replace($original_chars, $sanitized_chars, $filename);
 
 return $friendly_filename;
 
